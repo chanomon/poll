01ae6a6f40ba5d75f18215662d56d72a46f71547
chanomon <hdomeli@gmail.com>
2019-11-08 11:58:49 -0600

    class person

---
+++ person.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+#Copyright (C)  Elizandro Huipe Domratcheva. (elizandro@comunidad.unam.mx)
+#    Permission is granted to copy, distribute and/or modify this document
+#    under the terms of the GNU Free Documentation License, Version 1.3
+#    or any later version published by the Free Software Foundation;
+#    with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.
+#    A copy of the license is included in the section entitled "GNU
+#    Free Documentation License".
+
+
+class Person:
+    def __init__(self,age=18,income=1825,education=12,race,religion,marital_status,sexual_orientation,childrens=0,security_perception=10):
+        self.age = age #years
+        self.sex = sex #
+        self.income = income # dlls per year
+        self.education = education # years
+        self.race = race #race
+        self.religion = religion #religion
+        self.marital_status = marital_status
+        self.sexual_orientation = sexual_orientation
+        self.childrens = childrens}
+        self.security_perception = security_perception #0 insecure --> 10 secure
